c5ba50ba93e7b2627257aa84389b2d7950133978
Aprita Primayuda <aprita.primayuda@gmail.com>
2024-01-14 13:19:07 +0700

    Adding island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://prims-first-astro.netlify.app/",
-  integrations: [react()]
+  integrations: [react(), preact()]
 });
